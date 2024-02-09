2cad8f9a74a6fc2673dba2f1715aeb3461b065b5
shan3274 <khanshan949@gmail.com>
2024-02-09 17:47:25 +0530

    version v1.1

--- lib/e_button.dart
+++ lib/e_button.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
 class EButton extends StatelessWidget {
-  const EButton({super.key});
-
+  const EButton(
+      {super.key,
+      required this.pressed,
+      required this.num,
+      required this.isPressed});
+  final String num;
+  final bool isPressed;
+  final void Function() pressed;
+  @override
   Widget build(context) {
-    return Text("hello");
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+              backgroundColor: const Color.fromARGB(255, 209, 209, 209),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10))),
+          onPressed: isPressed ? pressed : null,
+          child: Text(
+            num,
+            style: const TextStyle(fontSize: 25, color: Colors.black),
+          )),
+    );
   }
 }
--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toc/game_board.dart';
 
 class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
@@ -12,6 +13,7 @@ class GameScreen extends StatefulWidget {
 class _GameScreen extends State<GameScreen> {
   var data = ['O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O'];
   var btnData = ["", "", "", "", "", "", "", "", ""];
+  var btnnum = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   var inc = 0;
 
   var wintext = "";
@@ -24,7 +26,11 @@ class _GameScreen extends State<GameScreen> {
           btnData[0] == "O" && btnData[4] == "O" && btnData[8] == "O" ||
           btnData[2] == "X" && btnData[4] == "X" && btnData[6] == "X" ||
           btnData[2] == "O" && btnData[4] == "O" && btnData[6] == "O") {
-        btnData = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+        for (var i = 0; i < btnData.length; i++) {
+          if (btnData[i] == "") {
+            btnData[i] = " ";
+          }
+        }
 
         wintext = "Completed";
       } else if (btnData[0] == "X" && btnData[3] == "X" && btnData[6] == "X" ||
@@ -33,297 +39,89 @@ class _GameScreen extends State<GameScreen> {
           btnData[1] == "O" && btnData[4] == "O" && btnData[7] == "O" ||
           btnData[2] == "X" && btnData[5] == "X" && btnData[8] == "X" ||
           btnData[2] == "O" && btnData[5] == "O" && btnData[8] == "O") {
-        btnData = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+        for (var i = 0; i < btnData.length; i++) {
+          if (btnData[i] == "") {
+            btnData[i] = " ";
+          }
+        }
 
         wintext = "Completed";
       } else if (btnData[3] == "X" && btnData[4] == "X" && btnData[5] == "X" ||
           btnData[3] == "O" && btnData[4] == "O" && btnData[5] == "O" ||
           btnData[6] == "X" && btnData[7] == "X" && btnData[8] == "X" ||
           btnData[6] == "O" && btnData[7] == "O" && btnData[8] == "O") {
-        btnData = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+        for (var i = 0; i < btnData.length; i++) {
+          if (btnData[i] == "") {
+            btnData[i] = " ";
+          }
+        }
 
         wintext = "Completed";
       }
     });
   }
-  
+
+  void pressed(int num) {
+    setState(() {
+      if (inc > 8) inc = 0;
+
+      btnData[num] = data[inc];
+      inc++;
+      win();
+    });
+  }
 
   @override
   Widget build(context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(mainAxisSize: MainAxisSize.min, children: [
-            const Text(
-              "Play O X",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor:
-                                const Color.fromARGB(255, 255, 255, 255),
-                            backgroundColor:
-                                const Color.fromARGB(255, 196, 87, 233),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                        onPressed: btnData[0] == ""
-                            ? () {
-                                setState(() {
-                                  if (inc > 8) inc = 0;
-
-                                  btnData[0] = data[inc];
-                                  inc++;
-                                  win();
-                                });
-                              }
-                            : null,
-                        child: Text(btnData[0])),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor:
-                                const Color.fromARGB(255, 255, 255, 255),
-                            backgroundColor:
-                                const Color.fromARGB(255, 196, 87, 233),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                        onPressed: btnData[1] == ""
-                            ? () {
-                                setState(() {
-                                  if (inc > 8) inc = 0;
-
-                                  btnData[1] = data[inc];
-                                  inc++;
-                                  win();
-                                });
-                              }
-                            : null,
-                        child: Text(btnData[1])),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor:
-                                const Color.fromARGB(255, 255, 255, 255),
-                            backgroundColor:
-                                const Color.fromARGB(255, 196, 87, 233),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                        onPressed: btnData[2] == ""
-                            ? () {
-                                setState(() {
-                                  if (inc > 8) inc = 0;
-
-                                  btnData[2] = data[inc];
-                                  inc++;
-                                  win();
-                                });
-                              }
-                            : null,
-                        child: Text(btnData[2])),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor:
-                                const Color.fromARGB(255, 255, 255, 255),
-                            backgroundColor:
-                                const Color.fromARGB(255, 196, 87, 233),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                        onPressed: btnData[3] == ""
-                            ? () {
-                                setState(() {
-                                  if (inc > 8) inc = 0;
-
-                                  btnData[3] = data[inc];
-                                  inc++;
-                                  win();
-                                });
-                              }
-                            : null,
-                        child: Text(btnData[3])),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor:
-                                const Color.fromARGB(255, 255, 255, 255),
-                            backgroundColor:
-                                const Color.fromARGB(255, 196, 87, 233),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                        onPressed: btnData[4] == ""
-                            ? () {
-                                setState(() {
-                                  if (inc > 8) inc = 0;
-
-                                  btnData[4] = data[inc];
-                                  inc++;
-                                  win();
-                                });
-                              }
-                            : null,
-                        child: Text(btnData[4])),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor:
-                                const Color.fromARGB(255, 255, 255, 255),
-                            backgroundColor:
-                                const Color.fromARGB(255, 196, 87, 233),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                        onPressed: btnData[5] == ""
-                            ? () {
-                                setState(() {
-                                  if (inc > 8) inc = 0;
-
-                                  btnData[5] = data[inc];
-                                  inc++;
-                                  win();
-                                });
-                              }
-                            : null,
-                        child: Text(btnData[5])),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor:
-                                const Color.fromARGB(255, 255, 255, 255),
-                            backgroundColor:
-                                const Color.fromARGB(255, 196, 87, 233),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                        onPressed: btnData[6] == ""
-                            ? () {
-                                setState(() {
-                                  if (inc > 8) inc = 0;
-
-                                  btnData[6] = data[inc];
-                                  inc++;
-                                  win();
-                                });
-                              }
-                            : null,
-                        child: Text(btnData[6])),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor:
-                                const Color.fromARGB(255, 255, 255, 255),
-                            backgroundColor:
-                                const Color.fromARGB(255, 196, 87, 233),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                        onPressed: btnData[7] == ""
-                            ? () {
-                                setState(() {
-                                  if (inc > 8) inc = 0;
-
-                                  btnData[7] = data[inc];
-                                  inc++;
-                                  win();
-                                });
-                              }
-                            : null,
-                        child: Text(btnData[7])),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor:
-                                const Color.fromARGB(255, 255, 255, 255),
-                            backgroundColor:
-                                const Color.fromARGB(255, 196, 87, 233),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10))),
-                        onPressed: btnData[8] == ""
-                            ? () {
-                                setState(() {
-                                  if (inc > 8) inc = 0;
-
-                                  btnData[8] = data[inc];
-                                  inc++;
-                                  win();
-                                });
-                              }
-                            : null,
-                        child: Text(btnData[8])),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                  foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-                  backgroundColor: const Color.fromARGB(255, 249, 5, 58),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10))),
-              onPressed: inc == 0
-                  ? null
-                  : () {
-                      setState(() {
-                        inc = 0;
-                        btnData = ["", "", "", "", "", "", "", "", ""];
-                        wintext = "";
-                      });
-                    },
-              label: const Text("Restart"),
-              icon: const Icon(Icons.restart_alt),
-            ),
-            Text(wintext)
-          ]),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                "Play O X",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              GameBoard(btnData: btnData, pressed: pressed, btnindex: btnnum),
+              Text(wintext),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                    backgroundColor: const Color.fromARGB(255, 249, 5, 58),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                onPressed: inc == 0
+                    ? null
+                    : () {
+                        setState(() {
+                          inc = 0;
+                          btnData = ["", "", "", "", "", "", "", "", ""];
+                          wintext = "";
+                        });
+                      },
+                label: const Text("Restart"),
+                icon: const Icon(Icons.restart_alt),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                      foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                      backgroundColor: Color.fromARGB(255, 5, 168, 249),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  },
+                  icon: const Icon(Icons.home),
+                  label: const Text("Home"))
+            ],
+          ),
         ),
       ),
     );
